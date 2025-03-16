b1cb3f93cc163da8c18125413e9366e928fc9bda
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-03-16 20:20:33 -0300

    add: new var env

--- Codigo/backend/src/app-config/app-config.schema.ts
+++ Codigo/backend/src/app-config/app-config.schema.ts
@@ -1,6 +1,10 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
+	API_LOG_LEVEL: Joi.string().default('level=info'),
+	API_PASSWORD_KEY_LENGTH: Joi.number().default(32),
+	API_PASSWORD_SALT_LENGTH: Joi.number().default(8),
+	API_JWT_SECRET: Joi.string().required(),
 	DB_TYPE: Joi.string().required(),
 	DB_NAME: Joi.string().required(),
 	DB_HOST: Joi.string().required(),
