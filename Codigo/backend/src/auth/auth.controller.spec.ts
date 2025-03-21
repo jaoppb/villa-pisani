d5b323293e170d8997a4b9fef72d7b28b5fbbe2f
jaoppb <joaopedroperes06@gmail.com>
2025-03-21 13:58:09 -0300

    fixed auth broken tests

--- Codigo/backend/src/auth/auth.controller.spec.ts
+++ Codigo/backend/src/auth/auth.controller.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { Repository } from 'typeorm';
+import { EncryptionModule } from 'src/encryption/encryption.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { JwtModule } from '@nestjs/jwt';
+import { AppConfigModule } from 'src/app-config/app-config.module';
+import { AppConfigService } from 'src/app-config/app-config.service';
+import { User } from 'src/user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from 'src/database/database.module';
 
 describe('AuthController', () => {
 	let controller: AuthController;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				EncryptionModule,
+				EventEmitterModule.forRoot(),
+				DatabaseModule,
+				TypeOrmModule.forFeature([User]),
+				JwtModule.registerAsync({
+					imports: [AppConfigModule],
+					inject: [AppConfigService],
+					useFactory: (configService: AppConfigService) => ({
+						secret: configService.JwtSecret,
+						signOptions: { expiresIn: '3d' },
+					}),
+				}),
+			],
 			controllers: [AuthController],
 			providers: [AuthService],
 		}).compile();
--- Codigo/backend/src/auth/auth.service.spec.ts
+++ Codigo/backend/src/auth/auth.service.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { EncryptionModule } from 'src/encryption/encryption.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { DatabaseModule } from 'src/database/database.module';
 
 describe('AuthService', () => {
 	let service: AuthService;
@@ -7,6 +12,12 @@ describe('AuthService', () => {
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			providers: [AuthService],
+			imports: [
+				EncryptionModule,
+				DatabaseModule,
+				TypeOrmModule.forFeature([User]),
+				EventEmitterModule.forRoot(),
+			],
 		}).compile();
 
 		service = module.get<AuthService>(AuthService);
