519b883bd39344f855d7698e631db425e9c29490
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-03-24 03:09:20 -0300

    feat: router

--- Codigo/frontend/src/app/app.routes.ts
+++ Codigo/frontend/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { Error404Component } from './pages/error404/error404.component';
+import { OutletComponent } from './pages/auth/outlet/outlet.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'home',
+	},
+	{
+		path: '',
+		loadChildren: () => 
+		import('./pages/home/home.routes').then(
+			(module_) => module_.routes,
+		),
+	},
+	{
+		path: 'auth',
+		component: OutletComponent,
+		// outlet: 'auth',
+		loadChildren: () =>
+			import('./pages/auth/auth.routes').then(
+				(module_) => module_.routes,
+			),
+	},
+	{
+		path: '404',
+		component: Error404Component
+	},
+	{
+		path: '**',
+		pathMatch: 'full',
+		redirectTo: '404'
+	}
+];
---
+++ Codigo/frontend/src/app/pages/home/home.routes.ts
@@ -0,0 +1,9 @@
+import { Routes, Route } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+
+export const routes = [
+	{
+		path: 'home',
+		component: HomeComponent,
+	}
+]
