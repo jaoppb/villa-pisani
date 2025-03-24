2c630d6b473c379a112b65dc21203b2dd70de1d4
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-03-24 03:07:36 -0300

    feat: config http client

---
+++ Codigo/frontend/src/app/interceptors/http-client.interceptor.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpInterceptorFn } from '@angular/common/http';
+
+
+describe('httpClientInterceptor', () => {
+  const interceptor: HttpInterceptorFn = (req, next) => 
+    TestBed.runInInjectionContext(() => httpClientInterceptor(req, next));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(interceptor).toBeTruthy();
+  });
+});
