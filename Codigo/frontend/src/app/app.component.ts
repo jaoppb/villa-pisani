bed86cec5d5f86241108db63ebe185b69919cdfd
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-03-24 03:06:52 -0300

    temp: home

--- Codigo/frontend/src/app/app.component.ts
+++ Codigo/frontend/src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeManagerService } from './services/theme-manager.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [ThemeManagerService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  constructor(private themeManagerService: ThemeManagerService) {}
+  
+  ngOnInit(): void {
+    this.themeManagerService.ngOnInit();
+  }
 }
