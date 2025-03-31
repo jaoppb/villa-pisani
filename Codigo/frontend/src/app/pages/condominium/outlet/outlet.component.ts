01079b600ad9d95aac30e60fc425261fd872fdb7
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-03-30 23:13:14 -0300

    feat: create file internal pages

---
+++ Codigo/frontend/src/app/pages/condominium/outlet/outlet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-outlet',
+  imports: [RouterOutlet],
+  templateUrl: './outlet.component.html',
+  styleUrl: './outlet.component.scss'
+})
+export class OutletComponent {
+
+}
