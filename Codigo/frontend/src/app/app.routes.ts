4ab3ae132d4dd12ce41f3790f685da1904880bb6
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-03-30 23:17:38 -0300

    feat: indexing pages

--- Codigo/frontend/src/app/app.routes.ts
+++ Codigo/frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Error404Component } from './pages/error404/error404.component';
-import { OutletComponent } from './pages/auth/outlet/outlet.component';
+import { OutletComponent as OutletAuht} from './pages/auth/outlet/outlet.component';
+import { OutletComponent as OutletCondominium } from './pages/condominium/outlet/outlet.component';
+import { AuthenticateGuard } from './guards/authenticate.guard';
+import { inject } from '@angular/core';
 
 export const routes: Routes = [
 	{
@@ -17,12 +20,21 @@ export const routes: Routes = [
 	},
 	{
 		path: 'auth',
-		component: OutletComponent,
-		// outlet: 'auth',
+		component: OutletAuht,
 		loadChildren: () =>
 			import('./pages/auth/auth.routes').then(
 				(module_) => module_.routes,
 			),
+	},
+	{
+		path: 'condominium',
+		component: OutletCondominium,
+		loadChildren: () =>
+			import('./pages/condominium/condominium.routes').then(
+				(module_) => module_.routes,
+			),
+		
+		canMatch: [() => inject(AuthenticateGuard).isLoggedIn()],
 	},
 	{
 		path: '404',
