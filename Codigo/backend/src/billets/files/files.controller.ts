669d1746e79718c05b5a7039d3adbea614a23f3c
jaoppb <joaopedroperes06@gmail.com>
2025-04-02 15:33:54 -0300

    implemented FilesController

--- Codigo/backend/src/billets/files/files.controller.ts
+++ Codigo/backend/src/billets/files/files.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	Delete,
+	Param,
+	Post,
+	UploadedFiles,
+	UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
 
 @Controller('billets/files')
-export class FilesController {}
+export class FilesController {
+	constructor(private readonly filesService: FilesService) {}
+
+	@Post(':billetId')
+	@UseInterceptors(FilesInterceptor)
+	upload(
+		@UploadedFiles() files: Array<Express.Multer.File>,
+		@Param('billetId') billetId: string,
+	) {
+		return this.filesService.uploadAll(billetId, files);
+	}
+
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.filesService.remove(id);
+	}
+}
