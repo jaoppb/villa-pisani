b700b0f04292a05a93254df2a12ea3f5bd144a07
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-04-07 03:14:30 -0300

    feat: has role

--- Codigo/frontend/src/app/services/accessToken.service.ts
+++ Codigo/frontend/src/app/services/accessToken.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { jwtDecode } from 'jwt-decode';
+import { payloadToken, Role } from "../model/user.model";
 
 @Injectable({
 	providedIn: 'root',
@@ -19,20 +20,53 @@ export class AccessTokenService {
 		localStorage.removeItem(this.KEY);
 	}
 
+	isTokenValid(token: string): boolean {
+		try {
+			const { exp } = jwtDecode<payloadToken>(token);
+			if (!exp) return false;
+
+			const currentTime = Math.floor(Date.now() / 1000);
+			return exp > currentTime;
+		} catch (error) {
+			return false;
+		}
+	}
+
 	get hasToken() {
 		const token = this.AccessToken;
 		if (!token) return false;
 
-		const { exp } = jwtDecode(token);
-		if (!exp) return false;
-
-		const expDate = new Date(exp * 1000);
-		const now = new Date();
-		
-		if (expDate < now) {
+		if (!this.isTokenValid(token)) {
 			this.removeAccessToken();
 			return false;
 		}
 		return true;
+	}
+
+	private hasRole(role: Role): boolean {
+		const token = this.AccessToken;
+		if (!token) return false;
+
+		if (!this.isTokenValid(token)) {
+			this.removeAccessToken();
+			return false;
+		}
+
+		const { roles } = jwtDecode<payloadToken>(token);
+		if (!roles) return false;
+
+		return Array.isArray(roles) && roles.includes(role);
+	}
+
+	get hasManager() {
+		return this.hasRole(Role.MANAGER);
+	}
+
+	get hasInhabitant() {
+		return this.hasRole(Role.INHABITANT);
+	}
+
+	get hasEmployee() {
+		return this.hasRole(Role.EMPLOYEE);
 	}
 }
