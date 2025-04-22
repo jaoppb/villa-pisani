ce1b13af36adbf88d1212f89cd98720771388327
jaoppb <joaopedroperes06@gmail.com>
2025-04-22 13:51:40 -0300

    added method UserService.updateRoles

--- Codigo/backend/src/user/user.service.ts
+++ Codigo/backend/src/user/user.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from 'src/auth/roles/role.entity';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	private readonly logger = new Logger(UserService.name);
+	constructor(
+		@InjectRepository(User)
+		private readonly userRepository: Repository<User>,
+	) {}
+
+	async updateRoles(userId: string, roles: Role[]) {
+		const user = await this.userRepository.findOneBy({ id: userId });
+		if (!user) {
+			this.logger.error('User not exists', userId);
+			throw new BadRequestException('User not exists');
+		}
+
+		this.logger.log('Updating user roles', userId, roles);
+		const updated = await this.userRepository.save({ id: userId, roles });
+		this.logger.log('User roles updated', userId, roles);
+
+		return {
+			id: updated.id,
+			roles: updated.roles,
+		};
+	}
+}
