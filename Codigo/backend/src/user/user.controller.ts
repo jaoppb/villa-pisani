86018c7c7d687d05200c2eaf625af2145abc9ccb
jaoppb <joaopedroperes06@gmail.com>
2025-04-22 13:53:29 -0300

    implement PATCH /users/permissions

--- Codigo/backend/src/user/user.controller.ts
+++ Codigo/backend/src/user/user.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	Patch,
+	Req,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { Roles } from 'src/auth/roles/role.decorator';
+import { Role } from 'src/auth/roles/role.entity';
+import { ChangePermissionDto } from './dto/permission.dto';
+import { Request } from 'src/http/request';
 
 @Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
+
+	@Patch('permissions')
+	@Roles(Role.MANAGER)
+	async updatePermissions(@Req() req: Request, @Body() body: ChangePermissionDto) {
+		const { user } = req;
+		if (user.id === body.userId && !body.roles.includes(Role.MANAGER)) {
+			throw new BadRequestException(
+				'You cannot remove your own manager role',
+			);
+		}
+
+		return await this.userService.updateRoles(body.userId, body.roles);
+	}
 }
