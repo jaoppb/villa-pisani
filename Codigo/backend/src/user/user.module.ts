1513e3d552fe18a7be4b00241c75a837ef339183
jaoppb <joaopedroperes06@gmail.com>
2025-04-23 13:00:40 -0300

    added EncryptionModule to UserModule

--- Codigo/backend/src/user/user.module.ts
+++ Codigo/backend/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { EncryptionModule } from 'src/encryption/encryption.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [EncryptionModule, TypeOrmModule.forFeature([User])],
 	controllers: [UserController],
 	providers: [UserService],
 })
