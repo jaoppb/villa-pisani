41286c0243488ba8fc05b9c25be74cc0f6e87f6a
jaoppb <joaopedroperes06@gmail.com>
2025-04-23 14:39:36 -0300

    added SafeUserDto constructor

--- Codigo/backend/src/user/dto/safe-user.dto.ts
+++ Codigo/backend/src/user/dto/safe-user.dto.ts
@@ -9,5 +9,19 @@ import { User } from '../entities/user.entity';
 
 export class SafeUserDto extends IntersectionType(
 	OmitType(UpdateUserDto, ['password'] as const),
-	PartialType(PickType(User, ['id', 'createAt', 'updateAt'])),
-) {}
+	PartialType(
+		PickType(User, ['id', 'createAt', 'updateAt', 'lastPasswordChange']),
+	),
+) {
+	constructor(user: User) {
+		super();
+		this.id = user.id;
+		this.name = user.name;
+		this.email = user.email;
+		this.birthDate = user.birthDate;
+		this.createAt = user.createAt;
+		this.updateAt = user.updateAt;
+		this.roles = user.roles;
+		this.lastPasswordChange = user.lastPasswordChange;
+	}
+}
