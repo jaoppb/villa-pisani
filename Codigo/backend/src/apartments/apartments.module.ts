22191625979096d56a956687b55fdff4dbadf85f
jaoppb <joaopedroperes06@gmail.com>
2025-04-25 17:04:37 -0300

    fix: added missing TypeOrm import to ApartmentsModule

--- Codigo/backend/src/apartments/apartments.module.ts
+++ Codigo/backend/src/apartments/apartments.module.ts
@@ -3,9 +3,10 @@ import { ApartmentsService } from './apartments.service';
 import { ApartmentsController } from './apartments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Apartment } from './entities/apartment.entity';
+import { Floor } from 'src/floors/entities/floor.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Apartment])],
+	imports: [TypeOrmModule.forFeature([Apartment, Floor])],
 	controllers: [ApartmentsController],
 	providers: [ApartmentsService],
 })
