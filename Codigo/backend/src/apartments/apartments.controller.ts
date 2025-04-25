8b9cbec82943809c2234884d86e390fc3f5dbe3f
jaoppb <joaopedroperes06@gmail.com>
2025-04-25 16:51:42 -0300

    added missing Roles checking for ApartmentsController methods

--- Codigo/backend/src/apartments/apartments.controller.ts
+++ Codigo/backend/src/apartments/apartments.controller.ts
@@ -2,17 +2,21 @@ import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { ApartmentsService } from './apartments.service';
 import { CreateApartmentDto } from './dto/create-apartment.dto';
 import { UpdateApartmentDto } from './dto/update-apartment.dto';
+import { Roles } from 'src/auth/roles/role.decorator';
+import { Role } from 'src/auth/roles/role.entity';
 
 @Controller('apartments')
 export class ApartmentsController {
 	constructor(private readonly apartmentsService: ApartmentsService) {}
 
 	@Post()
+	@Roles(Role.MANAGER)
 	create(@Body() createApartmentDto: CreateApartmentDto) {
 		return this.apartmentsService.create(createApartmentDto);
 	}
 
 	@Get()
+	@Roles(Role.MANAGER)
 	findAll() {
 		return this.apartmentsService.findAll();
 	}
@@ -23,6 +27,7 @@ export class ApartmentsController {
 	}
 
 	@Patch(':number')
+	@Roles(Role.MANAGER)
 	update(
 		@Param('number') number: number,
 		@Body() updateApartmentDto: UpdateApartmentDto,
