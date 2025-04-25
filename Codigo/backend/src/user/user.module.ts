9cfe05e36468fabf7c1a4ce2a77bc0e364821159
jaoppb <joaopedroperes06@gmail.com>
2025-04-25 16:38:20 -0300

    fix: added missing import for ApartmentRepository at UserModule

--- Codigo/backend/src/user/user.module.ts
+++ Codigo/backend/src/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { EncryptionModule } from 'src/encryption/encryption.module';
+import { Apartment } from 'src/apartments/entities/apartment.entity';
 
 @Module({
-	imports: [EncryptionModule, TypeOrmModule.forFeature([User])],
+	imports: [EncryptionModule, TypeOrmModule.forFeature([User, Apartment])],
 	controllers: [UserController],
 	providers: [UserService],
 	exports: [UserService],
