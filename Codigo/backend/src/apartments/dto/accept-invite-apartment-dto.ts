3f4e748b571f9434cb7594817575230525a351bb
jaoppb <joaopedroperes06@gmail.com>
2025-05-02 13:37:53 -0300

    fix: wrong usage if nested dto at AcceptInviteDto

--- Codigo/backend/src/apartments/dto/accept-invite-apartment-dto.ts
+++ Codigo/backend/src/apartments/dto/accept-invite-apartment-dto.ts
@@ -1,4 +1,5 @@
-import { IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsOptional, IsString, ValidateNested } from 'class-validator';
 import { SignUpAuthDto } from 'src/auth/dto/signup-auth.dto';
 
 export class AcceptInviteDto {
@@ -6,5 +7,7 @@ export class AcceptInviteDto {
 	inviteToken: string;
 
 	@IsOptional()
+	@ValidateNested()
+	@Type(() => SignUpAuthDto)
 	signUp?: SignUpAuthDto;
 }
