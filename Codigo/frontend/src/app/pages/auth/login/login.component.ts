4705a6db7b3b450062ee921c6fbcd8058e191fba
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-05-05 16:42:37 -0300

    feat: use invite

--- Codigo/frontend/src/app/pages/auth/login/login.component.ts
+++ Codigo/frontend/src/app/pages/auth/login/login.component.ts
@@ -3,21 +3,25 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { CustomInputComponent } from '../../../components/input/custom-input/custom-input.component';
 import { UserService } from '../../../services/user.service';
 import { MetaData } from '../../../services/meta-data.service';
+import { RouterModule } from '@angular/router';
+import { ApartmentService } from '../../../services/apartment.service';
 
 @Component({
   selector: 'app-login',
-  imports: [CustomInputComponent, ReactiveFormsModule],
+  imports: [CustomInputComponent, ReactiveFormsModule, RouterModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   form: FormGroup;
+  inviteParam: string | null = null;
 
 
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
+    private apartmentService: ApartmentService,
     private meta: MetaData,
   ) {
     this.meta.setMetaData({
@@ -29,11 +33,25 @@ export class LoginComponent {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
     });
+
+    const urlParams = new URLSearchParams(window.location.search);
+    this.inviteParam = urlParams.get('invite');
   }
 
-  submit(): void {
+  async submit() {
     this.userService.login(this.form.value).subscribe({
       next: () => {
+        if (this.inviteParam) {
+          console.log(this.inviteParam);
+          this.apartmentService.inviteUser(this.inviteParam).subscribe({
+            next: () => {
+              console.log('User invited successfully');
+            },
+            error: (err) => {
+              console.error('Failed to invite user:', err);
+            },
+          });
+        }
         window.location.href = '/condominium/home';
       },
       error: (err) => {
