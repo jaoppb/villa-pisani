39341fe213be085d72da9651f23fdb5e8193157f
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-05-05 17:57:18 -0300

    feat: user update dto

--- Codigo/frontend/src/app/model/user.model.ts
+++ Codigo/frontend/src/app/model/user.model.ts
@@ -1,50 +1,57 @@
 import { Apartments } from "./apartment.model";
 
 export interface loginRequest {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 export interface loginResponse {
-	accessToken: string;
+  accessToken: string;
 }
 
 export interface registerRequest {
-	name: string;
-	email: string;
-	birthDate: string;
-	password: string;
+  name: string;
+  email: string;
+  birthDate: string;
+  password: string;
 }
 
 export interface registerErrorResponse {
-	message: string;
-	error: string;
-	statusCode: number;
+  message: string;
+  error: string;
+  statusCode: number;
 }
 
 export enum Role {
-	MANAGER = 'manager',
-	INHABITANT = 'inhabitant',
-	EMPLOYEE = 'employee',
+  MANAGER = 'manager',
+  INHABITANT = 'inhabitant',
+  EMPLOYEE = 'employee',
 }
 
 export interface payloadToken {
-	email: string;
-	sub: string;
-	roles: Role[];
-	iss: string;
-	iat: number;
-	exp: number;
+  email: string;
+  sub: string;
+  roles: Role[];
+  iss: string;
+  iat: number;
+  exp: number;
 }
 
 export interface User {
-	id: string;
-	name: string;
-	email: string;
-	createAt: string;
-	updateAt: string;
-	birthDate: string | null;
-	roles: Role[];
-	lastPasswordChange: string;
-	apartment: Apartments | null;
+  id: string;
+  name: string;
+  email: string;
+  createAt: string;
+  updateAt: string;
+  birthDate: string | null;
+  roles: Role[];
+  lastPasswordChange: string;
+  apartment: Apartments | null;
+}
+
+export interface updateUser {
+  name?: string;
+  email?: string;
+  birthDate?: string | null;
+  roles?: Role[];
 }
