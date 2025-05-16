fdfac775bacba46c5a335b1efb83533b1ee8931c
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-05-16 05:35:38 -0300

    feat: modal user update

--- Codigo/frontend/src/app/pages/condominium/apartment/apartment.component.ts
+++ Codigo/frontend/src/app/pages/condominium/apartment/apartment.component.ts
@@ -8,75 +8,102 @@ import { User } from '../../../model/user.model';
 import { IconsComponent } from '../../../components/icons/iconBase/icons.component';
 import { ModalRemoveUserApartmentComponent } from '../../../components/modal/modal-remove-user-apartment/modal-remove-user-apartment.component';
 import { ModalApartmentInviteComponent } from '../../../components/modal/modal-apartment-invite/modal-apartment-invite.component';
+import { ModalUpdateUserComponent } from '../../../components/modal/modal-update-user/modal-update-user.component';
 
 @Component({
-  selector: 'app-apartment',
-  imports: [IconsComponent, RouterModule, ModalRemoveUserApartmentComponent, ModalApartmentInviteComponent],
-  templateUrl: './apartment.component.html',
-  styleUrl: './apartment.component.scss'
+	selector: 'app-apartment',
+	imports: [IconsComponent, RouterModule, ModalRemoveUserApartmentComponent, ModalApartmentInviteComponent, ModalUpdateUserComponent],
+	templateUrl: './apartment.component.html',
+	styleUrl: './apartment.component.scss'
 })
 export class ApartmentComponent {
-  apartment!: Apartments;
-  users: User[] = [];
-  isOpenModalRemoveUser: boolean = false;
-  isOpenModalInvite: boolean = false;
-  userToRemove!: User;
-  openModalInvites: boolean = false;
+	apartment!: Apartments;
+	users: User[] = [];
+	isOpenModalRemoveUser: boolean = false;
+	isOpenModalInvite: boolean = false;
+	userToRemove!: User;
+	openModalInvites: boolean = false;
+	IsOpenModalUpdateUser: boolean = false;
+	userUpdate!: User;
 
-  constructor(
-    private route: ActivatedRoute,
-    private apartmentService: ApartmentService,
-    private userService: UserService,
-    private tokenStorage: AccessTokenService
-  ) {
-    this.route.params.subscribe(params => {
-      let apartmentId = params['number'];
-      if (!apartmentId) {
-        this.userService.getUserLogin().subscribe((res: any) => {
-          if (res.body) {
-            apartmentId = res.body.apartment.number;
-          }
-        });
-      }
-      this.apartmentService.getApartment(apartmentId).subscribe((res: any) => {
-        if (res.body) {
-          this.apartment = res.body;
-          this.getUsers(res.body.number);
-        }
-      });
-    });
-  }
+	constructor(
+		private route: ActivatedRoute,
+		private apartmentService: ApartmentService,
+		private userService: UserService,
+		private tokenStorage: AccessTokenService
+	) {
+		this.route.params.subscribe(params => {
+			if (params['number']) {
+				const apartmentId = params['number'];
+				this.apartmentService.getApartment(apartmentId).subscribe((res: any) => {
+					if (res.body) {
+						this.apartment = res.body;
+						this.getUsers(res.body.number);
+					}
+				});
+			} else {
+				this.apartmentService.getApartmentLoginUser().subscribe((res: any) => {
+					if (res.body) {
+						this.apartment = res.body;
+						this.getUsers();
+					}
+				});
+			}
+		});
+	}
 
-  getUsers(apartmentId: number) {
-    this.apartmentService.getInhabitants(apartmentId).subscribe((res: any) => {
-      if (res.body) {
-        this.users = res.body
-      }
-    });
-  }
+	getUsers(apartmentId?: number) {
+		if (apartmentId !== undefined) {
+			this.apartmentService.getInhabitants(apartmentId).subscribe((res: any) => {
+				if (res.body) {
+					this.users = res.body;
+				}
+			});
+		} else {
+			this.apartmentService.getApartmentLoginUserInhabitants().subscribe((res: any) => {
+				if (res.body) {
+					this.users = res.body;
+				}
+			});
+		}
+	}
 
-  invite() {
-    this.isOpenModalInvite = true;
-    this.isOpenModalRemoveUser = false;
-  }
+	invite() {
+		this.isOpenModalInvite = true;
+		this.isOpenModalRemoveUser = false;
+	}
 
-  public isAdmin() {
-    return this.tokenStorage.hasManager;
-  }
+	public isAdmin() {
+		return this.tokenStorage.hasManager;
+	}
 
-  openModalRemoveUser(user: User): void {
-    this.userToRemove = user;
-    this.isOpenModalRemoveUser = true;
-  }
-  handleIsOpenRemoveUserChange(isOpen: boolean): void {
-    this.isOpenModalRemoveUser = isOpen;
-  }
+	openModalRemoveUser(user: User): void {
+		this.userToRemove = user;
+		this.isOpenModalRemoveUser = true;
+	}
+	handleIsOpenRemoveUserChange(isOpen: boolean): void {
+		this.isOpenModalRemoveUser = isOpen;
+	}
 
-  handleRemoveUser(userId: string): void {
-    this.users = this.users.filter(user => user.id !== userId);
-  }
+	handleRemoveUser(userId: string): void {
+		this.users = this.users.filter(user => user.id !== userId);
+	}
 
-  handleIsOpenInviteChange(isOpen: boolean): void {
-    this.isOpenModalInvite = isOpen;
-  }
+	handleIsOpenInviteChange(isOpen: boolean): void {
+		this.isOpenModalInvite = isOpen;
+	}
+
+	handleIsOpenUpdateUserChange(isOpen: boolean): void {
+		this.IsOpenModalUpdateUser = isOpen;
+	}
+
+	handleUpdateUser(user: User): void {
+		this.getUsers(this.apartment.number);
+	}
+
+	openModalUpdateUser(user: User) {
+		this.userUpdate = user;
+		this.IsOpenModalUpdateUser = true;
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
 }
