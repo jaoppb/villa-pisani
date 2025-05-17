71b7839487075302e53b8391f320cc027270380e
jaoppb <joaopedroperes06@gmail.com>
2025-05-17 11:47:11 -0300

    feat: mapped Bills entities

--- Codigo/backend/src/bills/entities/bill.entity.ts
+++ Codigo/backend/src/bills/entities/bill.entity.ts
@@ -1,1 +1,34 @@
-export class Bill {}
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	OneToOne,
+	PrimaryGeneratedColumn,
+	Relation,
+} from 'typeorm';
+import { BillFile } from '../files/entities/file.entity';
+import { Month } from './month.entity';
+
+@Entity()
+export class Bill {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column({ unique: true })
+	externalId: string;
+
+	@Column({ type: 'int' })
+	value: number;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@Column({ type: 'datetime' })
+	dueDate: Date;
+
+	@OneToOne(() => BillFile, (file) => file.bill)
+	file: Relation<BillFile>;
+
+	@Column({ type: 'set', enum: Month })
+	refer: Month;
+}
--- Codigo/backend/src/bills/files/entities/file.entity.ts
+++ Codigo/backend/src/bills/files/entities/file.entity.ts
@@ -1,3 +1,9 @@
+import { Bill } from 'src/bills/entities/bill.entity';
 import { File } from 'src/files/entities/file.entity';
+import { Entity, OneToOne, Relation } from 'typeorm';
 
-export class BillFile extends File {}
+@Entity()
+export class BillFile extends File {
+	@OneToOne(() => Bill, (bill) => bill.file)
+	bill: Relation<Bill>;
+}
