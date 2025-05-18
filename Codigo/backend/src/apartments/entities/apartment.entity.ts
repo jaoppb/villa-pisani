7228e0598df0c14c147842be9eb1702f9f6cd380
jaoppb <joaopedroperes06@gmail.com>
2025-05-17 22:15:28 -0300

    feat: added owner field to Apartment entity

--- Codigo/backend/src/apartments/entities/apartment.entity.ts
+++ Codigo/backend/src/apartments/entities/apartment.entity.ts
@@ -3,10 +3,13 @@ import { User } from 'src/user/entities/user.entity';
 import {
 	Column,
 	Entity,
+	JoinColumn,
 	JoinTable,
 	ManyToMany,
 	OneToMany,
+	OneToOne,
 	PrimaryColumn,
+	Relation,
 } from 'typeorm';
 
 @Entity('apartments')
@@ -19,6 +22,14 @@ export class Apartment {
 
 	@OneToMany(() => User, (user) => user.apartment)
 	inhabitants: User[];
+
+	@OneToOne(() => User, (user) => user.apartment, {
+		cascade: ['insert', 'update'],
+		eager: true,
+		nullable: true,
+	})
+	@JoinColumn({ referencedColumnName: 'id' })
+	owner?: Relation<User>;
 
 	@ManyToMany(() => Notice, (notice) => notice.apartments)
 	@JoinTable()
