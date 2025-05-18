4c2e9a94c9b800603eb840a43e1887e07366076e
jaoppb <joaopedroperes06@gmail.com>
2025-05-17 22:39:42 -0300

    feat: POST /bills

--- Codigo/backend/src/bills/bills.module.ts
+++ Codigo/backend/src/bills/bills.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BillsService } from './bills.service';
 import { BillsController } from './bills.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bill } from './entities/bill.entity';
+import { BillFile } from './files/entities/file.entity';
+import { AppConfigModule } from 'src/app-config/app-config.module';
+import { Apartment } from 'src/apartments/entities/apartment.entity';
+import { BillFilesService } from './files/files.service';
 
 @Module({
 	controllers: [BillsController],
 	providers: [BillsService, BillFilesService],
+	imports: [
+		AppConfigModule,
+		TypeOrmModule.forFeature([Bill, BillFile, Apartment]),
+	],
 })
 export class BillsModule {}
