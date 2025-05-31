da8fba91ff24bd65e3ce68357d54a3827c005f01
jaoppb <joaopedroperes06@gmail.com>
2025-05-31 16:15:03 -0300

    fix: parse-secret.sh
    using another folder as tmp due to read-only filesystem

--- Codigo/backend/scripts/parse-secret.sh
+++ Codigo/backend/scripts/parse-secret.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export TMPDIR=$(mktemp -d -p ./)
+
 parse() {
 	tmp=$(mktemp)
 	jq -r 'to_entries[] | "export \(.key)=\(.value | @sh)"' > ${tmp}
@@ -12,4 +14,4 @@ DATABASE_TEMP=$(echo $DATABASE | parse)
 source $API_TEMP
 source $DATABASE_TEMP
 
-rm -f $API_TEMP $DATABASE_TEMP
+rm -rf $TMPDIR
