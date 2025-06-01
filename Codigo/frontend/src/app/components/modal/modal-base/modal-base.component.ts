71df5d7e1363d185f1536d2ebf8b70dab8eaec5d
BGLuis <luishenrique.gbarbosa21@gmail.com>
2025-06-01 07:18:45 -0300

    feat: modal keybord navigation

--- Codigo/frontend/src/app/components/modal/modal-base/modal-base.component.ts
+++ Codigo/frontend/src/app/components/modal/modal-base/modal-base.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, Inject, PLATFORM_ID, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { IconsComponent } from '../../icons/iconBase/icons.component';
 
 @Component({
@@ -7,12 +7,50 @@ import { IconsComponent } from '../../icons/iconBase/icons.component';
   templateUrl: './modal-base.component.html',
   styleUrl: './modal-base.component.scss'
 })
-export class ModalBaseComponent {
+export class ModalBaseComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() isOpen: boolean = true;
   @Output() isOpenChange = new EventEmitter<boolean>();
+  @ViewChild('modalRoot') modalRoot!: ElementRef<HTMLDivElement>;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.handleIsOpenChange(true);
+  }
+
+  ngAfterViewInit() {
+    this.focusModal();
+  }
+
+  handleIsOpenChange(isOpen: boolean): void {
+    this.isOpen = isOpen;
+    this.isOpenChange.emit(this.isOpen);
+    if (!isOpen)
+      document.body.style.overflow = '';
+    else
+      document.body.style.overflow = 'hidden';
+  }
 
   closeModal() {
-    this.isOpen = false;
-    this.isOpenChange.emit(this.isOpen);
+    this.handleIsOpenChange(false);
   }
+
+  ngOnInit() {
+    window.addEventListener('keydown', this.handleEsc);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('keydown', this.handleEsc);
+  }
+
+  focusModal() {
+    if (this.isOpen && this.modalRoot) {
+      setTimeout(() => this.modalRoot.nativeElement.focus(), 0);
+      console.log('Modal focused');
+    }
+  }
+
+  handleEsc = (event: KeyboardEvent) => {
+    if (event.key === 'Escape' && this.isOpen) {
+      this.closeModal();
+    }
+  };
 }
