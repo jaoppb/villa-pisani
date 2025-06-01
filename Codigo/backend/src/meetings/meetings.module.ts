2271e55809ae943c58acc6f63cc2807717d31461
jaoppb <joaopedroperes06@gmail.com>
2025-06-01 16:29:13 -0300

    fix: added missing imports to MeetingModule

--- Codigo/backend/src/meetings/meetings.module.ts
+++ Codigo/backend/src/meetings/meetings.module.ts
@@ -5,10 +5,12 @@ import { MeetingFilesController } from './files/files.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Meeting } from './entities/meeting.entity';
 import { MeetingFile } from './files/entities/file.entity';
+import { FilesModule } from 'src/files/files.module';
+import { MeetingFilesService } from './files/files.service';
 
 @Module({
 	controllers: [MeetingsController, MeetingFilesController],
-	providers: [MeetingsService],
-	imports: [TypeOrmModule.forFeature([Meeting, MeetingFile])],
+	providers: [MeetingsService, MeetingFilesService],
+	imports: [TypeOrmModule.forFeature([Meeting, MeetingFile]), FilesModule],
 })
 export class MeetingsModule {}
