11a4670b4a306ee6858ad66a1c36ce793df46e02
jaoppb <joaopedroperes06@gmail.com>
2025-06-01 15:33:06 -0300

    feat: added typeorm import to MeetingModule

--- Codigo/backend/src/meetings/meetings.module.ts
+++ Codigo/backend/src/meetings/meetings.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { MeetingsService } from './meetings.service';
 import { MeetingsController } from './meetings.controller';
 import { MeetingFilesController } from './files/files.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Meeting } from './entities/meeting.entity';
+import { MeetingFile } from './files/entities/file.entity';
 
 @Module({
 	controllers: [MeetingsController, MeetingFilesController],
 	providers: [MeetingsService],
+	imports: [TypeOrmModule.forFeature([Meeting, MeetingFile])],
 })
 export class MeetingsModule {}
