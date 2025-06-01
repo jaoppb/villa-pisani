bc97226ef28214fc55fb2b2bc61b463898e80d4d
jaoppb <joaopedroperes06@gmail.com>
2025-06-01 15:34:25 -0300

    fix: removed findAll from meeting file service

--- Codigo/backend/src/meetings/files/files.service.ts
+++ Codigo/backend/src/meetings/files/files.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MeetingFilesService {
-	findAll() {
-		return `This action returns all files`;
-	}
-
 	findOne(id: number) {
 		return `This action returns a #${id} file`;
 	}
